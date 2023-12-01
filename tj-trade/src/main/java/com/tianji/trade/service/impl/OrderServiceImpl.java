f680f47f9a6868d4327acfb21bb4c81654af5317
xiaoqian <1805521303@qq.com>
2023-12-01 21:02:25 +0800

    解决订单删除的问题

--- tj-trade/src/main/java/com/tianji/trade/service/impl/OrderServiceImpl.java
+++ tj-trade/src/main/java/com/tianji/trade/service/impl/OrderServiceImpl.java
@@ -38,6 +38,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static com.tianji.common.constants.ErrorInfo.Msg.OPERATE_FAILED;
@@ -281,10 +282,11 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
             return;
         }
         // 3.判断订单所属用户与当前登录用户是否一致
-        if(userId != order.getUserId()){
+        if(!Objects.equals(userId, order.getUserId())){
             // 不一致，说明不是当前用户的订单，结束
             throw new BadRequestException("不能删除他人订单");
         }
+
         // 4.删除订单
         boolean success = removeById(id);
         if (!success) {
