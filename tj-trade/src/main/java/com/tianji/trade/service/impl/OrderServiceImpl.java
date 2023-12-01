b7304a66f888b2d8be38e96f85d3619ddce81d40
xiaoqian <1805521303@qq.com>
2023-12-01 21:08:08 +0800

    解决订单删除的问题

--- tj-trade/src/main/java/com/tianji/trade/service/impl/OrderServiceImpl.java
+++ tj-trade/src/main/java/com/tianji/trade/service/impl/OrderServiceImpl.java
@@ -286,7 +286,6 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
             // 不一致，说明不是当前用户的订单，结束
             throw new BadRequestException("不能删除他人订单");
         }
-
         // 4.删除订单
         boolean success = removeById(id);
         if (!success) {
