61594bdf8546e8e8cac153c37c60ae3e04e00b33
xiaoqian <1805521303@qq.com>
2024-02-03 21:25:53 +0800

    实现查询历史赛季列表接口

--- tj-learning/src/main/java/com/tianji/learning/controller/PointsBoardSeasonController.java
+++ tj-learning/src/main/java/com/tianji/learning/controller/PointsBoardSeasonController.java
@@ -1,9 +1,17 @@
 package com.tianji.learning.controller;
 
 
+import com.tianji.learning.domain.vo.PointsBoardSeasonVO;
+import com.tianji.learning.service.IPointsBoardSeasonService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,7 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-01-28
  */
 @RestController
-@RequestMapping("/points-board-season")
+@RequestMapping("/boards")
+@Api(tags = "赛季的相关接口")
+@RequiredArgsConstructor
 public class PointsBoardSeasonController {
+    private final IPointsBoardSeasonService pointsBoardSeasonService;
 
+    @ApiOperation("查询历史赛季列表")
+    @GetMapping("/seasons/list")
+    public List<PointsBoardSeasonVO> queryPointsBoardSeasons() {
+        return pointsBoardSeasonService.queryPointsBoardSeasons();
+    }
 }
--- tj-learning/src/main/java/com/tianji/learning/service/impl/PointsBoardSeasonServiceImpl.java
+++ tj-learning/src/main/java/com/tianji/learning/service/impl/PointsBoardSeasonServiceImpl.java
@@ -1,10 +1,18 @@
 package com.tianji.learning.service.impl;
 
+import com.tianji.common.utils.BeanUtils;
+import com.tianji.common.utils.CollUtils;
 import com.tianji.learning.domain.pojo.PointsBoardSeason;
+import com.tianji.learning.domain.vo.PointsBoardSeasonVO;
 import com.tianji.learning.mapper.PointsBoardSeasonMapper;
 import com.tianji.learning.service.IPointsBoardSeasonService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -17,4 +25,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class PointsBoardSeasonServiceImpl extends ServiceImpl<PointsBoardSeasonMapper, PointsBoardSeason> implements IPointsBoardSeasonService {
 
+    /**
+     * 查询历史季列表，必须是当前赛季之前的（开始时间小于等于当前时间）
+     */
+    @Override
+    public List<PointsBoardSeasonVO> queryPointsBoardSeasons() {
+        LocalDateTime now = LocalDateTime.now();
+        List<PointsBoardSeason> pointsBoardSeasons = lambdaQuery()
+                .le(PointsBoardSeason::getBeginTime, now)
+                .list();
+        if (CollUtils.isNotEmpty(pointsBoardSeasons)) {
+            return pointsBoardSeasons.stream()
+                    .map(e -> BeanUtils.copyBean(e, PointsBoardSeasonVO.class))
+                    .collect(Collectors.toList());
+        }
+        return CollUtils.emptyList();
+    }
 }
