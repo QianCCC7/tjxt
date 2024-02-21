ef01390add4e39cfd0b4610ac11c26e9281d996b
xiaoqian <1805521303@qq.com>
2024-02-21 17:24:38 +0800

    使用EnumMap优化简单工厂模式改造分布式锁

--- tj-promotion/src/main/java/com/tianji/promotion/utils/MyRedisLockFactory.java
+++ tj-promotion/src/main/java/com/tianji/promotion/utils/MyRedisLockFactory.java
@@ -1,13 +1,10 @@
 package com.tianji.promotion.utils;
 
-import com.tianji.common.exceptions.BizIllegalException;
-import lombok.RequiredArgsConstructor;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
 
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -15,12 +12,12 @@ import static com.tianji.promotion.utils.MyRedisLockType.*;
 
 @Component
 public class MyRedisLockFactory {
-    private final RedissonClient redissonClient;
     private final Map<MyRedisLockType, Function<String, RLock>> lockHandlers;
 
     public MyRedisLockFactory(RedissonClient redissonClient) {
-        this.redissonClient = redissonClient;
-        this.lockHandlers = new HashMap<>(MyRedisLockType.values().length);
+        // 只有当Key是枚举类型时可以使用EnumMap，其底层不是hash表，而是简单的数组。
+        // 由于枚举项数量固定，因此这个数组长度就等于枚举项个数，然后按照枚举项序号作为角标依次存入数组。这样就能根据枚举项序号作为角标快速定位到数组中的数据。
+        this.lockHandlers = new EnumMap<>(MyRedisLockType.class);
         this.lockHandlers.put(RE_ENTRANT_LOCK, redissonClient::getLock);
         this.lockHandlers.put(FAIR_LOCK, redissonClient::getFairLock);
         this.lockHandlers.put(READ_LOCK, name -> redissonClient.getReadWriteLock(name).readLock());
